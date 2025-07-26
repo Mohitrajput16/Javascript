0bd50a2d693e33afabff46e4f615d06a5d2d9dfc
Mohitrajput16 <163308492+Mohitrajput16@users.noreply.github.com>
2025-07-26 11:10:45 +0000

    changes done

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,2 @@
 console.log("Mohit")
+console.log("Rajput")
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=9344
+let accEmail="rajput@gmail.com"
+var accPassword="3455"
+acccity="Indore"
+
+// accountId=32982
+
+accEmail="mr@.com"
+accPassword="123"
+acccity="dewas"
+console.log(accountId);
+console.table([accountId,accEmail,accPassword,acccity])
