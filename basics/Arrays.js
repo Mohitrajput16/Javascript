663278f50213268fa7c81317aac59a64d09031e8
Mohitrajput16 <163308492+Mohitrajput16@users.noreply.github.com>
2025-07-27 12:55:46 +0000

    Add initial implementation of array operations in Arrays.js

---
+++ basics/Arrays.js
@@ -0,0 +1,14 @@
+const arr=[0,4,3,6,true,"mohit"];
+
+const arr2= new Array(3,54,5,6)
+console.log(arr[2]);
+
+arr.push(94)
+arr.pop()
+arr.unshift(8)
+arr.shift(8)
+
+
+console.log(arr.includes(3))
+console.log(arr.indexOf(3))
+console.log(arr)
